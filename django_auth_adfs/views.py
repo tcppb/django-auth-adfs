280e82fa77b3823c1a2067f83d706e6c31ac10cd
Teofil Cojocariu <teofil.cojocariu@paddypowerbetfair.com>
2019-04-09 11:56:41 +0300

    Fixes.

--- django_auth_adfs/views.py
+++ django_auth_adfs/views.py
@@ -4,9 +4,10 @@ from django.contrib.auth import authenticate, login
 from django.http.response import HttpResponse
 from django.shortcuts import redirect
 from django.views.generic import View
-from django.utils.http import is_safe_url, urlsafe_base64_decode
+from django.utils.http import is_safe_url
 
 from django_auth_adfs.config import settings
+from django_auth_adfs.util import _decode_url
 
 
 class OAuth2View(View):
@@ -27,7 +28,7 @@ class OAuth2View(View):
                 login(request, user)
 
                 if request.GET.get('state'):  # currently this is only the "next" URL
-                    next_url = urlsafe_base64_decode(request.GET.get('state')).decode()
+                    next_url = _decode_url(request.GET.get('state'))
                     if django.VERSION < (1, 11):
                         hosts_arg = {'host': request.get_host()}
                     else:
